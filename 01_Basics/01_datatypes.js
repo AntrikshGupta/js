5ea8fdeceb4db5f62152134455ad56cbb2904eb2
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-02-05 08:44:46 +0000

    datatype commit 1

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code in newer version
+
+//alert(3+3) cant use as we are using node js not a browser
+
+console.log(9+
+    9
+)
+
+// can use the above format but code readability is compromised, it should be highly readable
+
+let name = "Hello"  // string
+let age = 20        // integer
+let value= true     // boolean
+let state;
+// null - standalone value
+// undefined - state is undefined
+// symbol - uniqueness
+
+
+// object 
+
+
+console.log(typeof name)        //string
+console.log(typeof age)         //number
+console.log(typeof value)       //boolean
+console.log(typeof state)       //undefined
+console.log(typeof null)        //object
+
+
