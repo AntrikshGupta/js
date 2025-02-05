4247a61c2b54fd099f293a93ea7c19f45144d60d
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-02-05 07:38:11 +0000

    var commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "antriksh@army.org"
+var accountPassword = "676767"
+accountCity="Indore"
+
+let accountRecovery;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountRecovery])
+
+// accountId=2 //not allowed
+
+accountEmail="ag@gmail.com"
+accountPassword="878787"
+accountCity="Noida"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountRecovery])
+
+accountRecovery="What is your pet name?"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountRecovery])
