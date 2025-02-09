9b482ebf505ef5815d186842e20cdef64e0357f5
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-02-09 09:54:54 +0000

    conversions_test

--- 01_Basics/03_conversion_operations.js
+++ 01_Basics/03_conversion_operations.js
@@ -6,3 +6,34 @@ let valueInNumber = Number(score)
 
 console.log(typeof(valueInNumber));
 
+console.log(valueInNumber);
+
+let score1="33abc"
+
+let valueInNumber1 = Number(score1)
+
+console.log(typeof(valueInNumber1));
+
+console.log(valueInNumber1);
+
+/*      score -> valueInNumber
+        33 -> 33
+        "33" -> 33
+        "33abc"/"world" -> NaN
+        true/false -> 1/0
+        undefined -> undefined
+        null -> 0
+*/
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+/*
+    1 -> true
+    0 -> false
+    "" -> false
+    "world" -> true
+    
+*/
+
