06710891952f547663ff5bee5fe54ec93f143dd5
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-03-12 20:48:51 +0000

    firts

---
+++ 01_Basics/07_DateTime.js
@@ -0,0 +1,19 @@
+
+
+const date = new Date()
+console.log(date.getTimezoneOffset);
+
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(typeof(date));
+
+const createDate = new Date(2023, 0 , 23)
+const createDate1 = new Date(2023, 2 , 13)
+const createDate2 = new Date(2023, 36 , 23)
+console.log(createDate);
+console.log(createDate1);
+console.log(createDate2);
+
+
